d73c7358d92ca9a7a90454458897a09273cbc672
bibaxhenis <xbxhenis@gmail.com>
2020-11-11 23:08:59 +0000

    Adding Calculator Project

---
+++ Core/Calculator/Calculator/CalculatorLib.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Calculator
+{
+    public class CalculatorLib
+    {
+
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+
+        public static int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+
+        public static int Divide(int x, int y)
+        {
+            return x / y;
+        }
+
+        public static int Module(int x, int y)
+        {
+            return x % y;
+        }
+    }
+}
+
+
---
+++ Core/Calculator/CalculatorTests/StaticCalculatorTest.cs
@@ -0,0 +1,120 @@
+using NUnit.Framework;
+using Calculator;
+
+namespace CalculatorTests
+{
+    public class StaticCalculatorTest       
+    {
+       private int _addResult, _subtractResult, _multiplyResult, _divideResult, _moduleResult;
+
+        [SetUp]
+        public void Setup()
+        {
+            _addResult = CalculatorLib.Add(3, 5);           // Calls The CalculatorLib.Add Method with parameters of (3,5) and assigns the output to the _addResult variable.
+            _subtractResult = CalculatorLib.Subtract(8, 8); // Calls The CalculatorLib.Subtract Method with parameters of (8,8) and assigns the output to the _subtractResult variable.
+            _multiplyResult = CalculatorLib.Multiply(9, 2); // Calls The CalculatorLib.Multiply Method with parameters of (9,2) and assigns the output to the _multiplyResult variable.
+            _divideResult = CalculatorLib.Divide(8, 2);     // Calls The CalculatorLib.Divide Method with parameters of (8,2) and assigns the output to the _divideResult variable.
+            _moduleResult = CalculatorLib.Module(6, 6);     // Calls The CalculatorLib.Module Method with parameters of (6,6) and assigns the output to the _moduleResult variable.
+        }
+        // Creating Tests For the CalculatorLib.Add Method With Fixed Parameters: (3,5) Result Should Be 8
+        [Test]   // |---Test Begins---|
+        public void IsAddWorking()
+        {
+            Assert.AreEqual(8, _addResult);  // |-----Assert-----|
+        }
+
+        //Creating a Test for the CalculatorLib.Add Method with 4 Test Cases.
+        [Test]                 // |---Test Begins---|
+        [TestCase(4,5,9)]      // |Test Case No. 01.|
+        [TestCase(-4,5,1)]     // |Test Case No. 02.|
+        [TestCase(-3,-8,-11)]  // |Test Case No. 03.|
+        [TestCase(232,-232,0)] // |Test Case No. 04.|
+        public void IsAddWorkingWithTestCases(int a, int b, int expected) // This Method Will Be Tested 4 Times; One Time Per Test Case.
+        {
+            int testResult = CalculatorLib.Add(a, b); // |Arrange , Assert|
+            Assert.AreEqual(expected, testResult);    // |-----Assert-----|
+        }
+
+        //Creating a Test for the Calculator.Subtract method with Fixed Parameters: (8,8) Result Should Be 0
+        [Test]   // |---Test Begins---|
+        public void IsSubtractWorking()
+        {
+            Assert.AreEqual(0, _subtractResult);  // |-----Assert-----|
+        }
+
+        //Creating a Test for the Calculator.Subtract Method with 5 Test Cases.
+        [Test]                  // |---Test Begins---|
+        [TestCase(4,3,1)]       // |Test Case No. 01.|
+        [TestCase(6,-7,13)]     // |Test Case No. 02.|
+        [TestCase(-13,20,-33)]  // |Test Case No. 03.|
+        [TestCase(125,25,100)]  // |Test Case No. 04.|
+        [TestCase(23,23,0)]     // |Test Case No. 05.|
+        public void IsSubtractWorkingWithTestCases(int a, int b, int expected) // This Method Will Be Tested 5 Times; One Time Per Test Case.
+        {
+            int testResult = CalculatorLib.Subtract(a,b); // |Arrange , Assert|
+            Assert.AreEqual(expected, testResult);        // |-----Assert-----|
+
+        }
+
+        //Creating a Test for the Calculator.Multiply Method With Fixed Parameters: (9,2) Result Should Be 18
+        [Test]   // |---Test Begins---|
+        public void IsMultiplyWorking()
+        {
+            Assert.AreEqual(18, _multiplyResult); // |-----Assert-----|
+        }
+
+        //Creating a Test for the Calculator.Multiply Method with 4 Test Cases. 
+        [Test]                     // |---Test Begins---|
+        [TestCase(4, 3, 12)]       // |Test Case No. 01.|
+        [TestCase(6, -7, -42)]     // |Test Case No. 02.|
+        [TestCase(-13, 20, -260)]  // |Test Case No. 03.|
+        [TestCase(125, 25, 3125)]  // |Test Case No. 04.|
+        public void IsMultiplyWorkingWithTestCases(int a, int b, int expected) // This Method Will Be Tested 4 Times; One Time Per Test Case.
+        {
+            int testResult = CalculatorLib.Multiply(a, b); // |Arrange , Assert|
+            Assert.AreEqual(expected, testResult);         // |-----Assert-----|
+
+        }
+
+        //Creating a Test for the Calculator.Divide Method With Fixed Parameters: (8,2) Result Should Be 4
+        [Test]   // |---Test Begins---|
+        public void IsDivideWorking()
+        {
+            Assert.AreEqual(4, _divideResult); // |-----Assert-----|
+        }
+
+        //Creating a Test for the Calculator.Divide Method with 4 Test Cases.
+        [Test]                    // |---Test Begins---|
+        [TestCase(4, 3, 1)]       // |Test Case No. 01.|
+        [TestCase(5, 6, 0)]       // |Test Case No. 02.|
+        [TestCase(-13, -13, 1)]   // |Test Case No. 03.|
+        [TestCase(125, 25, 5)]    // |Test Case No. 04.|
+        public void IsDivideWorkingWithTestCases(int a, int b, int expected) // This Method Will Be Tested 4 Times; One Time Per Test Case.
+        {
+            int testResult = CalculatorLib.Divide(a, b); // |Arrange , Assert|
+            Assert.AreEqual(expected, testResult);       // |-----Assert-----|
+
+        }
+
+        //Creating a Test for the Calculator.Module Method With Fixed Parameters: (6,6) Result Should Be 0.
+        [Test]   // |---Test Begins---|
+        public void IsModule()
+        {
+            Assert.AreEqual(0, _moduleResult); // |-----Assert-----|
+        }
+
+        //Creating a Test for the Calculator.Module Method with 4 Test Cases. 
+        [Test]                    // |---Test Begins---|
+        [TestCase(4, 3, 1)]       // |Test Case No. 01.|
+        [TestCase(5, 6, 5)]       // |Test Case No. 02.|
+        [TestCase(-13, -13, 0)]   // |Test Case No. 03.|
+        [TestCase(125, 124, 1)]   // |Test Case No. 04.|
+        public void IsModuleWorkingWithTestCases(int a, int b, int expected) // This Method Will Be Tested 4 Times; One Time Per Test Case.
+        {
+            int testResult = CalculatorLib.Module(a, b); // |Arrange , Assert|
+            Assert.AreEqual(expected, testResult);       // |-----Assert-----|
+
+        }
+
+    }
+}
